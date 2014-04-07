e8a06beb131f5be3c7b66be673514188d6c8658d
Artemis <lp859@hotmail.fr>
2014-04-07 03:01:10 +0200

    First version of Akadok's main file with loading and stuff

---
+++ akadok.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bitbucket.org/deimosgame/go-akadok/util"
+)
+
+const (
+	configFile = "server.cfg"
+)
+
+var (
+	config *akadokConfig
+	log    *util.Logger
+)
+
+func main() {
+	/* Config loading */
+
+	loadConfig()
+
+	/* Logging Engine */
+
+	log = util.InitLogging(config.LogFile)
+	// Log everything to file
+	log.ToFile = true
+	// Change debug mode if needed
+	log.DebugMode = config.Verbose
+	log.Notice("Akadok is loading...")
+
+	log.Notice("Akadok has started")
+}
