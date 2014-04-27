83e95a14483df37d4f194f985b5865afa8f8566c
Artemis <lp859@hotmail.fr>
2014-04-27 12:01:30 +0200

    Added support for auto packet splitting according to the IPv4 MTU (576 bytes). Example of usage (for 578 bytes):
    	pkt, pktdata := packet.NewPacket(5), []byte("Hello worldHello worldHello
    worldHello worldHello worldHello worldHello worldHello worldHello
    worldHello worldHello worldHello worldHello worldHello worldHello
    worldHello worldHello worldHello worldHello worldHello worldHello
    worldHello worldHello worldHello worldHello worldHello worldHello
    worldHello worldHello worldHello worldHello worldHello worldHello
    worldHello worldHello worldHello worldHello worldHello worldHello
    worldHello worldHello worldHello worldHello worldHello worldHello
    worldHello worldHello worldHello worldHello worldHello worldHello
    worldHello worldHe")
    	pkt.AddField(&pktdata)
    	fmt.Println(packet.ReadPackets(*pkt.Encode()...))
    
    The result of ReadPackets(Encode()) is identical to pkt value.

---
+++ packet/multipacket.go
@@ -0,0 +1,36 @@
+package packet
+
+import (
+	"bytes"
+	"errors"
+)
+
+func ReadPackets(receivedPackets ...*[]byte) (*Packet, error) {
+	decodedPackets := make([]*Packet, len(receivedPackets))
+	// Reorder received packets
+	for _, currentPacket := range receivedPackets {
+		decodedPacket, err := ReadPacket(currentPacket)
+		if err != nil {
+			return nil, err
+		}
+		decodedPackets[decodedPacket.Index] = decodedPacket
+	}
+	// Put all packets together
+	dataBuffer := bytes.NewBuffer(nil)
+	for _, currentPacket := range decodedPackets {
+		dataBuffer.Write(currentPacket.Data)
+	}
+	decodedPackets[0].Data, decodedPackets[0].Total = dataBuffer.Bytes(), 1
+	return decodedPackets[0], nil
+}
+
+func (p *Packet) IsSplitted() bool {
+	return p.Total > 1
+}
+
+func IsSplitted(p []byte) (bool, error) {
+	if len(p) < 4 {
+		return false, errors.New("Invalid packet")
+	}
+	return p[3] > 1, nil
+}
--- packet/packet.go
+++ packet/packet.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Packet struct {
-	Id   byte
-	Data []byte
+	Id, Index, Total byte
+	Data             []byte
 }
 
 // NewPacket creates an empty packet with its id
@@ -18,33 +18,35 @@ func NewPacket(id byte) *Packet {
 }
 
 // ReadPacket reads a byte array contents and tries to parse it as a packet
-func ReadPacket(packetBuffer []byte) (*Packet, error) {
-	if len(packetBuffer) < 3 {
+func ReadPacket(packetBuffer *[]byte) (*Packet, error) {
+	if len(*packetBuffer) < 3 {
 		return &Packet{}, errors.New("Invalid packet!")
 	}
 
 	// Checksum
-	packetChecksum := packetBuffer[0]
-	if packetChecksum != byte(len(packetBuffer)%256) {
+	packetChecksum := (*packetBuffer)[0]
+	if packetChecksum != byte(len(*packetBuffer)%256) {
 		return &Packet{}, errors.New("Invalid checksum (corrupted packet?)")
 	}
 
 	packet := &Packet{
-		Id:   packetBuffer[1],
-		Data: packetBuffer[2:],
+		Id:    (*packetBuffer)[1],
+		Index: (*packetBuffer)[2],
+		Total: (*packetBuffer)[3],
+		Data:  (*packetBuffer)[4:],
 	}
 	return packet, nil
 }
 
 // AddField adds a new field at the end of the current packet
-func (p *Packet) AddField(fieldData []byte) error {
+func (p *Packet) AddField(fieldData *[]byte) error {
 	packetBuffer := bytes.NewBuffer(p.Data)
 	if p.Data != nil {
 		if err := packetBuffer.WriteByte(0); err != nil {
 			return err
 		}
 	}
-	if _, err := packetBuffer.Write(fieldData); err != nil {
+	if _, err := packetBuffer.Write(*fieldData); err != nil {
 		return err
 	}
 	p.Data = packetBuffer.Bytes()
@@ -53,11 +55,11 @@ func (p *Packet) AddField(fieldData []byte) error {
 
 // AddFieldBytes is an alias for AddField, except it doesn't needs an array
 func (p *Packet) AddFieldBytes(b ...byte) {
-	p.AddField(b)
+	p.AddField(&b)
 }
 
 // GetField returns the value of a field using its index
-func (p *Packet) GetField(n int) ([]byte, error) {
+func (p *Packet) GetField(n int) (*[]byte, error) {
 	// Begining of the field
 	i := 0
 	for j := 0; j < n; i++ {
@@ -76,18 +78,40 @@ func (p *Packet) GetField(n int) ([]byte, error) {
 			break
 		}
 	}
-	return p.Data[start:j], nil
+	field := p.Data[start:j]
+	return &field, nil
 }
 
 // Encode encodes the packets to a byte array in order to send it on the network
-func (p *Packet) Encode() *[]byte {
-	// Checksum
-	checksum := byte((len(p.Data) + 2) % 256)
-	// Buffer
-	buf := bytes.NewBuffer(nil)
-	buf.WriteByte(checksum)
-	buf.WriteByte(p.Id)
-	buf.Write(p.Data)
-	result := buf.Bytes()
-	return &result
+func (p *Packet) Encode() *[]*[]byte {
+	if len(p.Data) <= 572 {
+		// Checksum
+		checksum := byte((len(p.Data) + 4) % 256)
+		// Buffer
+		buf := bytes.NewBuffer(nil)
+		buf.WriteByte(checksum)
+		buf.WriteByte(p.Id)
+		buf.WriteByte(p.Index)
+		buf.WriteByte(p.Total)
+		buf.Write(p.Data)
+		result := buf.Bytes()
+		return &[]*[]byte{&result}
+	}
+	// Splitted packet
+	packetCount := len(p.Data)/572 + 1
+	packets := make([]*[]byte, packetCount)
+	for i := 0; i < packetCount; i++ {
+		currentPacketLen := 572
+		if len(p.Data)-i*572 < 572 {
+			currentPacketLen = len(p.Data)
+		}
+		currentPacket := Packet{
+			Id:    p.Id,
+			Index: byte(i),
+			Total: byte(packetCount),
+			Data:  p.Data[i*572 : currentPacketLen],
+		}
+		packets[i] = (*currentPacket.Encode())[0]
+	}
+	return &packets
 }
