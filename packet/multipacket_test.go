a2971518402dd49f09219d93c6fa4089456064fb
Artemis <lp859@hotmail.fr>
2014-04-27 12:38:26 +0200

    Added testing for multiple packet reading and encoding

---
+++ packet/multipacket_test.go
@@ -0,0 +1,24 @@
+package packet
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestReadPackets(t *testing.T) {
+	// 578 bytes packet
+	pkt, pktdata := NewPacket(5), []byte("Hello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHe")
+	pkt.AddField(&pktdata)
+	result, err := ReadPacket(*pkt.Encode()...)
+	if err != nil || bytes.Compare(result.Data, pktdata) != 0 {
+		t.Fail()
+	}
+}
+
+func TestSplitted(t *testing.T) {
+	pkt, pktdata := NewPacket(5), []byte("Hello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHe")
+	pkt.AddField(&pktdata)
+	if !pkt.IsSplitted() {
+		t.Fail()
+	}
+}
