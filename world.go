aeb7e434c5d632a8c978755c01dd4ceacf10eaa6
Artemis <lp859@hotmail.fr>
2014-05-02 17:32:52 +0200

    Added auto encoding of a world's players

--- world.go
+++ world.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"bitbucket.org/deimosgame/go-akadok/packet"
+	"bytes"
+	"encoding/binary"
+	"reflect"
 	"time"
 )
 
 type World struct {
 	players  []*Player
 	entities []*Entity
+}
+
+func (w *World) Packet() {
+	p := packet.New(0x04)
+	if len(w.players) > 0 {
+		playerPrefix := byte('A')
+		p.AddFieldBytes(playerPrefix)
+		for _, player := range players {
+			val := reflect.ValueOf(*player).Elem()
+			for i := 0; i < val.NumField(); i++ {
+				fieldValue, fieldType := val.Field(i).Interface(),
+					val.Type().Field(i)
+				fieldTag := fieldType.Tag
+				prefix := []byte(fieldTag.Get("prefix"))
+				if len(prefix) == 0 {
+					continue
+				}
+				p.AddField(&prefix)
+				switch fieldType.Type.String() {
+				case "string":
+					valueBytes := []byte(fieldValue.(string))
+					p.AddField(&valueBytes)
+				case "float32":
+					buf := bytes.NewBuffer(nil)
+					binary.Write(buf, binary.LittleEndian, fieldValue.(float32))
+					bufBytes := buf.Bytes()
+					p.AddField(&bufBytes)
+				}
+			}
+		}
+	}
 }
 
 // WorldSimulation does all the world simulation work
